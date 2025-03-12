80136d53f9dc3e7315e61f52d966feb1f0240e9a
Davi Reis <reisdavi805@gmail.com>
2025-03-12 20:00:13 -0300

    Update animation.js

--- src/Js/animation.js
+++ src/Js/animation.js
@@ -11,3 +11,14 @@ function digitarTexto() {
 }
 
 window.onload = digitarTexto;
+
+let elemScroll = document.querySelector(".scroll");
+let elemContainer = elemScroll.querySelector(".scroll__container");
+let elemFilhos = Array.from(elemContainer.children);
+
+elemFilhos.forEach(item => {
+  let elemDuplicado = item.cloneNode(true);
+  elemDuplicado.setAttribute('aria-hidden', 'true');
+  
+  elemContainer.appendChild(elemDuplicado);
+})
